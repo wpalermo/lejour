e12ea81e65d74e3c5170156da3e4ac328fd74ab0
William Palermo <william.palermo@accenture.com>
2018-05-16 15:01:08 -0300

    gravacao em arquivo OK. Faltam mais comentarios.

--- wpalermo/src/main/java/com/lejour/wpalermo/DesafioSilhuetas.java
+++ wpalermo/src/main/java/com/lejour/wpalermo/DesafioSilhuetas.java
@@ -10,38 +10,40 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Hello world!
+ * 
+ * @author william.palermo
  *
  */
-public class App2 {
 
-	final static String PATH = "./src/main/resources/casos/caso1.txt";
+
+public class DesafioSilhuetas {
+
+	final static String RESOURCE_PATH = "./src/main/resources/casos/";
+	final static String FILE_NAME = "caso1.txt";
+	final static String RESULT_FILE = FILE_NAME.split("\\.")[0] + "_RESULTADO.txt";
 
 	private static Integer qtdCasos = 0;
 	private static Integer linhasCount = 0;
+	private static Stream<String> lines;
 
 	public static void main(String[] args) throws IOException {
 
 		List<List<Integer>> values = new ArrayList<List<Integer>>();
 
-		Path path = Paths.get(PATH);
+		Path readPath = Paths.get(RESOURCE_PATH + FILE_NAME);
+		
+		lines = Files.lines(readPath);
 
-		Stream<String> lines = Files.lines(path);
+		
+		List<String> casos = lines.map(String::valueOf).collect(Collectors.toList());
+		
+		qtdCasos = Integer.valueOf(casos.remove(0));
 
-		List<String> teste = lines.map(String::valueOf).collect(Collectors.toList());
-
-		qtdCasos = Integer.valueOf(teste.remove(0));
-
-		teste.forEach(line -> {
+		casos.forEach(line -> {
 
 			linhasCount++;
 
-			Integer tamanhoCaso = 0;
-
-			if (linhasCount % 2 == 1)
-				tamanhoCaso = Integer.valueOf(line);
-
-			else {
+			if (linhasCount % 2 != 1) {
 				List<Integer> list = new ArrayList<Integer>();
 				for (String s : line.split(" "))
 					list.add(Integer.valueOf(s));
@@ -53,13 +55,11 @@ public class App2 {
 		if (linhasCount / 2 != qtdCasos)
 			System.out.println("WARN - Quantidade de linhas diferente da quantidade passada no header");
 
-		List<Integer> returnable = new ArrayList<Integer>();
+		List<String> returnable = new ArrayList<String>();
 
 		for (List<Integer> lista : values) {
 
-			Integer index = 0;
 			Integer valorParcial = 0;
-			Integer maior = 0;
 			Integer indexMaior = 0;
 			Integer indexSegundoMaior = 0;
 			Integer valor = 0;
@@ -71,8 +71,6 @@ public class App2 {
 				indexMaior = buscarIndexMaior(lista, i);
 
 				if (indexMaior == null) {
-					// System.out.println("Nao tem maior, procurar decrescente");
-
 					if (i + 1 == lista.size())
 						break;
 
@@ -83,11 +81,10 @@ public class App2 {
 						continue;
 					else {
 
-						maior = lista.get(indexSegundoMaior);
 						for (Integer j : lista.subList(i + 1, indexSegundoMaior))
 							valorParcial += valor - j;
 
-						System.out.println(valorParcial);
+						//System.out.println(valorParcial);
 						i = indexSegundoMaior - 1;
 
 					}
@@ -95,24 +92,33 @@ public class App2 {
 				} else if (indexMaior == (i + 1))
 					continue;
 				else {
-					maior = lista.get(indexMaior);
 					for (Integer j : lista.subList(i + 1, indexMaior))
 						valorParcial += valor - j;
 
-					System.out.println(valorParcial);
+					//System.out.println(valorParcial);
 					i = indexMaior - 1;
 				}
 
 			}
 
-			returnable.add(valorParcial);
+			returnable.add(valorParcial.toString());
 
 		}
 
-		System.out.println("-------RESPOSTA------");
-		returnable.forEach(System.out::println);
+		//System.out.println("-------RESPOSTA------");
+		//returnable.forEach(System.out::println);
+		
+		Files.write(Paths.get(RESOURCE_PATH + RESULT_FILE), returnable);
 	}
 
+	
+	/**
+	 * Metodo procura na lista se existe alguma outra coluna maior do que a atual.
+	 * E verifica se essa coluna vem depois da atual na seguencia. 
+	 * @param lista
+	 * @param index
+	 * @return
+	 */
 	private static Integer buscarIndexMaior(List<Integer> lista, Integer index) {
 
 		Integer count = 0;
@@ -128,6 +134,13 @@ public class App2 {
 
 	}
 
+	/**
+	 * No caso de ja ter encontrado a maior coluna, esse metodo eh responsavel por encontrar a segunda maior. 
+	 * A partir desse momento, a segunda maior passa a ser considerada como a maior e a logica segue a mesma.
+	 * @param lista
+	 * @param index
+	 * @return
+	 */
 	private static Integer buscarIndexSegundoMaior(List<Integer> lista, Integer index) {
 
 		Integer count = index;
@@ -135,6 +148,7 @@ public class App2 {
 
 		Integer segundoMaior = lista.subList(index + 1, lista.size()).stream().max(Integer::compare).get();
 
+		//If para o caso de ter 2 numeros iguais seguidos e esse ser igual ao maior
 		if (segundoMaior == numero) {
 			count = index+1;
 			for (Integer i : lista.subList(index + 1, lista.size())) {
